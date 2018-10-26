00bb6057a49a10a73c6d07ec913f624ffefd14d6
Martin Caetano <pelcasandra@me.com>
2018-10-26 11:01:11 -0300

    Comment charge.succeeded event from initializer.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,6 +6,6 @@ StripeEvent.configure do |events|
   # Listen to the charge event to make sure we get non-subscription
   # purchases as well. Invoice is only for subscriptions and manual creation
   # so it does not include individual charges.
-  events.subscribe 'charge.succeeded', Pay::Stripe::ChargeSucceeded.new
+  # events.subscribe 'charge.succeeded', Pay::Stripe::ChargeSucceeded.new
   events.subscribe 'charge.refunded', Pay::Stripe::ChargeRefunded.new
 end
